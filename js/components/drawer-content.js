05f08f1c85d323a12a9d3033c6c09e17b91b3cc5
Evan Siroky <evan.siroky@yahoo.com>
2017-05-12 18:07:09 -0700

    Only render certain menu items in drawer menu

--- js/components/drawer-content.js
+++ js/components/drawer-content.js
@@ -14,13 +14,63 @@ import {
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 import {addNavigationHelpers, DrawerItems, DrawerNavigator} from 'react-navigation'
 
+import type { DrawerScene } from 'react-navigation/src/views/Drawer/DrawerView'
 
 type Props = {
-
+  getLabel: (scene: DrawerScene) => ?(React.Element<*> | string),
+  navigation: Object,
+  renderIcon: (scene: DrawerScene) => ?React.Element<*>,
 }
+
+const routesToRenderInMenu = ['Home', 'Settings']
+const activeTintColor = '#2196f3'
+const activeBackgroundColor = 'rgba(0, 0, 0, .04)'
+const inactiveTintColor = 'rgba(0, 0, 0, .87)'
+const inactiveBackgroundColor = 'transparent'
 
 export default class DrawerContent extends Component {
   props: Props
+
+  _renderMenuItems () {
+    const {getLabel, navigation, renderIcon} = this.props
+    const itemsToRender = []
+    navigation.state.routes.map((route: *, index: number) => {
+      if (routesToRenderInMenu.indexOf(route.key) === -1) return
+      const focused = navigation.state.index === index
+      const color = focused ? activeTintColor : inactiveTintColor
+      const backgroundColor = focused
+        ? activeBackgroundColor
+        : inactiveBackgroundColor
+      const scene = { route, index, focused, tintColor: color }
+      const icon = renderIcon(scene)
+      const label = getLabel(scene)
+      itemsToRender.push(
+        <TouchableOpacity
+          key={route.key}
+          onPress={() => {
+            navigation.navigate('DrawerClose');
+            navigation.navigate(route.routeName);
+          }}
+        >
+          <View style={[styles.menuItem, { backgroundColor }]}>
+            {icon
+              ? <View
+                  style={[styles.menuIcon, focused ? null : styles.menuIconInactive]}
+                >
+                  {icon}
+                </View>
+              : null}
+            {typeof label === 'string'
+              ? <Text style={[styles.menuLabel, { color }]}>
+                  {label}
+                </Text>
+              : label}
+          </View>
+        </TouchableOpacity>
+      )
+    })
+    return itemsToRender
+  }
 
   render () {
     return (
@@ -31,7 +81,7 @@ export default class DrawerContent extends Component {
             style={styles.cfazLogo}
             />
         </View>
-        <DrawerItems {...this.props} />
+        {this._renderMenuItems()}
         <TouchableOpacity
           onPress={() => Linking.openURL('http://carfreenearme.com/')}
           >
@@ -84,5 +134,21 @@ const styles = StyleSheet.create({
   logoContainer: {
     height: 100,
     width: 300
+  },
+  menuIcon: {
+    marginHorizontal: 16,
+    width: 24,
+    alignItems: 'center',
+  },
+  menuIconInactive: {
+    opacity: 0.62
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  menuLabel: {
+    margin: 16,
+    fontWeight: 'bold',
   }
 })
